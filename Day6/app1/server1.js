e2e8a467c39044bc8f8213152f1879557fdaef85
Saeel <saeeldhatrak007@gmail.com>
2022-11-08 18:20:31 +0530

    day 6 ended

--- Day6/app1/server1.js
+++ Day6/app1/server1.js
@@ -2,11 +2,17 @@ const express = require('express')
 
 const app = express()
 
+
+// used to parse the request body contents
 const bodyParser = require('body-parser')
 
 const mysql = require('mysql')
 
-// app.use(bodyParser)
+// use body parser's json parser to parse body into json Object
+// when you call json() it returns an object of a function
+// which can be used for parsing body to the json object
+app.use(bodyParser.json())
+// unless we add this app.use middleware, your'e body will not be read by the express
 
 app.get('/product', (request, response) => {
 
@@ -37,12 +43,98 @@ app.get('/product', (request, response) => {
 })
 
 
-// app.post('/product', (request, response) => {
+app.post('/product', (request, response) => {
 
-//     console.log('body: ')
+    const connection = mysql.createConnection({
+        hots: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'firstdb'
+    })
 
-    
-// })
+    connection.connect()
+
+    console.log(`body: `)
+    console.log(request.body)
+
+    const statement = `insert into product1
+                       (title, description, price,
+                        category, company) values
+                       ('${request.body.title}',
+                       '${request.body.description}',
+                       '${request.body.price}',
+                       '${request.body.category}',
+                       '${request.body.company}')`
+
+    connection.query(statement, (error, data) => {
+
+        connection.end()
+
+        if(error){
+            response.end('error has occuered')
+        }
+        else{
+            console.log(data)
+            response.send(`data added succesfully`)
+        }
+    })
+})
+
+
+app.put('/product', (request, response) => {
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'firstdb'
+    })
+
+    connection.connect()
+
+    const statement = `update product1 set title = '${request.body.title}' where id = '${request.body.id}'`
+
+
+    connection.query(statement, (error, data) => {
+
+        connection.end()
+
+        if(error){
+            response.end('error has occured while updating')
+        }
+        else{
+            response.send('updated successfully')
+        }
+    })
+})
+
+
+app.delete('/product', (request, response) => {
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'firstdb'
+    })
+
+    connection.connect()
+
+    const statement = `delete from product1 where id = '${request.body.id}'`
+
+
+    connection.query(statement, (error, data) => {
+
+        connection.end()
+
+        if(error){
+            response.end('error has occured while deleting')
+        } 
+        else{
+            response.send('deleted successfully')
+        }
+    })
+})
 
 
 app.listen('3000', '0.0.0.0', () => {
